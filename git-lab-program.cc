42a89afe92ed00431ae8f7bf7831f807e73fd23c
Ze Ali <78237371+xZeshan@users.noreply.github.com>
2022-05-14 23:41:40 -0400

    Add files via upload
    @<Nasseef>, I'm done

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Ze Ali>
+ *        Date: <05/14/2022>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
